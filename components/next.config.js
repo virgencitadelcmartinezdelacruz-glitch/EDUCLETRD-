dd48ec358214e0b49df6eb2443a56d19c8f4e4d3
Virgencita Martinez <tucorreo@example.com>
2026-09-07 04:18:22 -0400

    Configurar correctamente next.config.js en la raiz

---
+++ components/next.config.js
@@ -0,0 +1,16 @@
+import withPWA from 'next-pwa';
+
+const withPWAConfig = withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+});
+
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+
+export default withPWAConfig(nextConfig);
