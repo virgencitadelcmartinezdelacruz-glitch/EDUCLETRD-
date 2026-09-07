41e7e88cff8c7232ff81a46e20fe2de86b806450
Virgencita Martinez <tucorreo@example.com>
2026-09-07 04:22:09 -0400

    Usar unicamente next.config.js para produccion

--- components/next.config.js
+++ components/next.config.js
@@ -1,16 +1,8 @@
-import withPWA from 'next-pwa';
-
-const withPWAConfig = withPWA({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development',
-});
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
 };
 
-export default withPWAConfig(nextConfig);
+export default nextConfig;
